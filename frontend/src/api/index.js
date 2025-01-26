3337032cf5085d5e21913e75d4845cb0ec29ccc8
hariom8710 <hariom.kushwahabtechaiml22@smslucknow.ac.in>
2025-01-26 23:20:24 +0530

    Normalize line endings

---
+++ frontend/src/api/index.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+// http://localhost:8080/api/
+const API = axios.create({
+  // baseURL: "https://mern-image-generator.onrender.com/api",
+  baseURL: "http://localhost:5000/api",
+});
+export const GetPosts = async () => await API.get("/post");
+export const CreatePost = async (data) => await API.post("/post", data);
+export const GenerateImageFromPrompt = async (data) =>
+  await API.post("/generateImage", data);
